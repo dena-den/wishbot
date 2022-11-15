2965385b54f5514cf94ec34a5b3e2df0dec4ea28
Denis Bulychev <denis_bulychev@epam.com>
2022-11-16 00:45:33 +0700

    Fixes.

--- app/const/queries.py
+++ app/const/queries.py
@@ -9,7 +9,7 @@ QUERY_WISHES_RESERVED_BY_ME = """
         JOIN wishlists wl ON wl.id = wh.wish_id	
                         AND wh.tg_id_who_chose = {tg_id}
         JOIN users u ON u.id = wl.user_id 
-    WHERE CONVERT_TZ(NOW(),'+00:00','+03:00') BETWEEN wh.start_datetime AND IFNULL(wh.end_datetime , STR_TO_DATE('01-01-2099', '%d-%m-%Y'))
+    WHERE wh.end_datetime IS NULL
 """
 
 
